1f186bcfaf64a7dba057940b3be3c16ba1ceec70
Madeline Barrowman <maddiebarrowman@Madelines-Air.ip>
2024-04-05 02:15:08 +1300

    Added initial files for the project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/<y2ass1>">
       <App />
     </BrowserRouter>
   </React.StrictMode>
